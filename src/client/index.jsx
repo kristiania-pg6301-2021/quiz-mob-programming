a6eaa9a9bb3a45d8884e6ad495feee6d275e4d8f
venici2606 <venicia_p@live.no>
2021-03-17 15:47:47 +0100

    fetch question from server

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import {getQuiz} from "./getQuiz";
 
 
 function Quiz() {
-    
+    useEffect(async () => {
+        const res = await fetch("/api/question");
+        if (!res.ok) {
+            console.log("error", res)
+        }
+
+        const question = await res.json()
+        console.log(question)
+
+    })
     function handleClick(answerIndex) {
         if(answerIndex === quiz.indexOfRightAnswer) {
             alert("Correct answer");
--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,11 @@ const {getQuiz} = require("./getQuiz");
 console.log("hello world")
 
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")))
 
 app.listen(3001, () => {
     console.log("http://localhost:3001")
