8aeea0d631df26308c4c73c1929dc176ee2b1662
Waligustav <wgbjork@gmail.com>
2021-03-17 15:03:35 +0100

    Extracting getQuiz

--- src/index.jsx
+++ src/index.jsx
@@ -1,60 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const quizzes = [
-    {
-        question: "What kind of language is JavaScript?",
-        answers: [
-            "Strongly and statically typed",
-            "Strongly and dynamically typed",
-            "Weakly and statically typed",
-            "Weakly and dynamically typed",
-        ],
-        indexOfRightAnswer: 3
-    },
-    {
-        question: "In JavaScript, what is the result of the following?\n\n+(!![]+!![]+!![]+!![]+[]+(!![]+!![]))",
-        answers: [
-            "Compilation exception",
-            "Runtime exception",
-            "42",
-            "'42'"
-        ],
-        indexOfRightAnswer: 2
-    },
-    {
-        question: "In JavaScript, what is the result of the following?\n\n[3,18,1,2].sort()\n",
-        answers: [
-            "[1, 2, 3, 18]",
-            "[1, 18, 2, 3]",
-            "[18, 1, 2, 3]",
-            "Runtime exception"
-        ],
-        indexOfRightAnswer: 1
-    },
-    {
-        question: "In JavaScript, what is the result  of the following?\n\nfalse + true?",
-        answers: [
-            "false",
-            "true",
-            "'falsetrue'",
-            "1"
-        ],
-        indexOfRightAnswer: 3
-    },
-    {
-        question: "What is Babel mainly used for?",
-        answers: [
-            "To transpile code into valid JS code",
-            "To bundle together the code of different JS files",
-            "To download third-party dependencies",
-            "To run test cases"
-        ],
-        indexOfRightAnswer: 0
-    }
-];
-
-
+import {getQuiz} from "./getQuiz";
 
 
 function Quiz() {
@@ -68,7 +14,7 @@ function Quiz() {
         
     }
     
-    const quiz = quizzes[0];
+    const quiz = getQuiz();
     return <>
     <h1>{quiz.question}</h1>
         {quiz.answers.map((a, index) => <button onClick={() => handleClick(index)}>{a}</button>)}
