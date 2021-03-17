ed295a196d22f0e785157743af6235ae8b7a5e76
Petter Haugland <petterhaugland@gmail.com>
2021-03-17 16:09:53 +0100

    Add prettier

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,39 +1,43 @@
-import React, {useEffect, useState} from 'react'
-import ReactDOM from 'react-dom'
-import {getQuiz} from "./getQuiz";
-
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { getQuiz } from "./getQuiz";
 
 function Quiz() {
-    const [quiz, setQuiz] = useState()
-    useEffect(async () => {
-        const res = await fetch("/api/question");
-        if (!res.ok) {
-            console.log("error", res)
-        }
-
-        const question = await res.json()
-
-        console.log(question)
-
-        setQuiz(question)
-    }, [])
-
-    function handleClick(answerIndex) {
-        if(answerIndex === quiz.indexOfRightAnswer) {
-            alert("Correct answer");
-        } else {
-            alert("Wrong answer");
-        }
-        
+  const [quiz, setQuiz] = useState();
+  useEffect(async () => {
+    const res = await fetch("/api/question");
+    if (!res.ok) {
+      console.log("error", res);
     }
-    
-    if(!quiz){
-        return <div>Loading...</div>
+
+    const question = await res.json();
+
+    console.log(question);
+
+    setQuiz(question);
+  }, []);
+
+  function handleClick(answerIndex) {
+    if (answerIndex === quiz.indexOfRightAnswer) {
+      alert("Correct answer");
+    } else {
+      alert("Wrong answer");
     }
-    return <>
-    <h1>{quiz.question}</h1>
-        {quiz.answers.map((a, index) => <button onClick={() => handleClick(index)} key={index}>{a}</button>)}
+  }
+
+  if (!quiz) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <h1>{quiz.question}</h1>
+      {quiz.answers.map((a, index) => (
+        <button onClick={() => handleClick(index)} key={index}>
+          {a}
+        </button>
+      ))}
     </>
+  );
 }
 
-ReactDOM.render(<Quiz/>, document.getElementById("root"))
+ReactDOM.render(<Quiz />, document.getElementById("root"));
--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,18 @@
-const {getQuiz} = require("./getQuiz");
+const { getQuiz } = require("./getQuiz");
 
-console.log("hello world")
+console.log("hello world");
 
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "..", "..", "dist")))
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 
 app.listen(3001, () => {
-    console.log("http://localhost:3001")
-})
+  console.log("http://localhost:3001");
+});
 
-app.get("/api/question", ((req, res) => {
-    res.json(getQuiz())
-}))
+app.get("/api/question", (req, res) => {
+  res.json(getQuiz());
+});
